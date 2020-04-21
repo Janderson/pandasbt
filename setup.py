e04320d95fce547643500db250aee2c48cb5ee1b
Janderson <Janderson Ferreira>
2020-04-21 12:18:03 -0300

    missed_file, of last update

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import setuptools
 
 with open("README.md", "r") as r:
     long_description = r.read()
-long_description = ""
 
 setuptools.setup(
     name="pandasbt",
-    version="0.1.0",
+    version="0.2.0",
     author="Janderson FFerreira",
     author_email="ffjanderson@gmail.com",
     description="Simple framework to test your strategies ideias with pandas",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/Janderson/pandasbt",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
